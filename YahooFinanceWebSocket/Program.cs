8760da780135fcce33a7eb603d4db2f59d3ed8db
izzeww <izzew@hemmadatorn>
2024-01-17 00:03:53 +0100

    Fixed a lot of issues, added stock tickers. Still buggy though, stops after a few minutes and doesn't write anything, then it writes a lot if you stop the application (CTRL+C)

--- YahooFinanceWebSocket/Program.cs
+++ YahooFinanceWebSocket/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.WebSockets;
+﻿using ProtoBuf;
+using System.Net.WebSockets;
 using System.Text;
 
 namespace YahooFinanceWebSocket
@@ -7,24 +8,22 @@ namespace YahooFinanceWebSocket
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
             using (ClientWebSocket client = new ClientWebSocket())
             {
                 Uri serviceUri = new Uri("wss://streamer.finance.yahoo.com/");
                 var cancellationToken = new CancellationTokenSource();
-                cancellationToken.CancelAfter(TimeSpan.FromSeconds(120));
+                cancellationToken.CancelAfter(TimeSpan.FromSeconds(1200));
 
                 try
                 {
                     await client.ConnectAsync(serviceUri, cancellationToken.Token);
-                    string message = "{\"subscribe\":[\"ETH-USD\", \"BTC-USD\", \"YM=F\", \"ES=F\", \"QM=F\", \"RTY=F\", \"MNQ=F\", \"NQ=F\"]}";
+                    string message = "{\"subscribe\":[\"ETH-USD\", \"BTC-USD\", \"YM=F\", \"ES=F\", \"QM=F\", \"RTY=F\", \"MNQ=F\", \"NQ=F, \"\"^GSPC\", \"INVE-B.ST\", \"^OMX\", \"SBB-B.ST\"]}";
                     ArraySegment<byte> byteToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
 
                     if (client.State == WebSocketState.Open)
                     {
+                        Console.WriteLine("Connection successful.");
                         await client.SendAsync(byteToSend, WebSocketMessageType.Text, true, cancellationToken.Token);
-                        Console.WriteLine("test");
                         var responseBuffer = new byte[1024];
                         var offset = 0;
                         var packet = 1024;
@@ -33,8 +32,11 @@ namespace YahooFinanceWebSocket
                             ArraySegment<byte> byteReceived = new ArraySegment<byte>(responseBuffer, offset, packet);
                             WebSocketReceiveResult response = await client.ReceiveAsync(byteReceived, cancellationToken.Token);
                             var responseMessage = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
-                            Console.WriteLine(responseMessage); // this prints the response in base64
-                                                                // protobuf-encoded form to the console, works fine
+
+                            var x = Convert.FromBase64String(responseMessage);
+                            var y = Serializer.Deserialize<PricingData>((ReadOnlySpan<byte>)x);
+
+                            Console.WriteLine($"{y.Id}: {y.Price}, at {DateTimeOffset.FromUnixTimeMilliseconds(y.Time).DateTime}");
                         }
                     }
                 }
@@ -43,6 +45,5 @@ namespace YahooFinanceWebSocket
             }
             Console.ReadLine();
         }
-
     }
 }
